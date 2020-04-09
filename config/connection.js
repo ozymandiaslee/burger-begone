cf49cc7b9a139bdc665f664b961fdc214b2fce62
Ozymandias <raymondlee@kaffeinatenc.com>
2020-04-09 18:42:44 -0400

    New Push

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
 var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-  mysql.createConnection({
+ connection= mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -17,13 +16,7 @@ else {
 }
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
